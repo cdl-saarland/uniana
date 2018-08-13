8cda3089afba213f347886f37b32451920206a3f
Sebastian Hack <hack@cs.uni-saarland.de>
2018-08-13 16:30:53 +0200

    more reasonable unch; essentially like dominance now

--- analysis.v
+++ analysis.v
@@ -1,4 +1,5 @@
 Require Import Coq.Classes.EquivDec.
+Require Import Coq.Classes.RelationClasses.
 Require Import Coq.Bool.Bool.
 Require Import Coq.Logic.Eqdep_dec.
 Require Import Coq.Logic.Decidable.
@@ -27,6 +28,8 @@ Module Uniana.
 
   Parameter init_uni : Var -> Prop.
   Parameter start_tag : Tag.
+  Parameter all_lab : set Lab.
+  Parameter all_lab_spec : forall l, set_In l all_lab.
 
   Definition States := State -> Prop.
   Definition Coord := prod Lab Tag.
@@ -99,6 +102,7 @@ Module Uniana.
 
   Notation "p -->* q" := (p === q \/ p --> q) (at level 70, right associativity).
 
+  Definition is_def_lab x p := exists q, is_def x q p = true.
 
   Lemma Conf_dec :
     forall (x y : Conf), {x = y} + {x <> y}.
@@ -503,11 +507,21 @@ Module Uniana.
   Definition Unch := Lab -> Var -> set Lab.
 
   Definition unch_concr (unch : Unch) : Traces :=
-    fun k => fun t => forall to i s u x, (In k t (to, i, s) ->
-                               set_In u (unch to x) ->
-                               exists j r, Precedes k t (u, j, r) (to, i, s) /\
-                                      r x = s x).
+    fun k => fun t => forall to i s u x, In k t (to, i, s) ->
+                              set_In u (unch to x) ->
+                              exists j r, Precedes k t (u, j, r) (to, i, s) /\
+                                     r x = s x.
 
+  (*
+  Definition unch_concr (unch : Unch) : Traces :=
+    fun k => fun t => forall to i s u x, In k t (to, i, s) ->
+                              set_In u (unch to x) ->
+                              exists d dj dr j r, is_def_lab x d /\
+                                             Precedes k t (d, dj, dr) (u, j, r) -> 
+                                             Precedes k t (u, j, r) (to, i, s) /\
+                                             r x = s x.
+  *)
+                           
   Definition unch_join_ptw (d d' : set Lab) := set_inter Lab_dec' d d'. 
 
   Definition unch_join (u u' : Unch) : Unch :=
@@ -517,49 +531,89 @@ Module Uniana.
     set_add Lab_dec' p (if is_def x q p then empty_set Lab else unch q x).
 
   Definition unch_trans_ptw (unch : Unch) l x : set Lab :=
-    let f := fun q acc => unch_join_ptw q acc in
+    if Lab_dec' l root then set_add Lab_dec' root (empty_set Lab) else
+    let f := fun q acc => set_inter Lab_dec' q acc in
     let t := fun q => unch_trans_local unch q l x in
-    fold_right f (empty_set Lab) (map t (preds l)).
+    fold_right f all_lab (map t (preds l)).
       
   Definition unch_trans (unch : Unch) : Unch :=
     fun l x => unch_trans_ptw unch l x.
 
-  Lemma unch_trans_root : forall unch x, unch_trans unch root x = empty_set Lab.
+  Lemma unch_trans_root : forall unch x, unch_trans unch root x = set_add Lab_dec' root (empty_set Lab).
   Proof.
     intros.
     cut (preds root = nil); intros.
     + unfold unch_trans, unch_trans_ptw.
-      rewrite H.
-      simpl.
-      reflexivity.
+      destruct (Lab_dec' root root); firstorder.
     + cut (forall q, ~ List.In q (preds root)); intros; eauto using list_emp_in, root_no_pred. 
   Qed.
 
-  Inductive Front (u : Unch) : Var -> Lab -> Type :=
-    | FrontDef : forall x p, (exists q, is_def x q p = true) -> Front u x p
-    | FrontIter : forall x l l' r r' p, Front u x l ->
+  Inductive Front (u : Unch) : Var -> Lab -> Prop :=
+    | FrontDef : forall x p, is_def_lab x p -> Front u x p
+    | FrontIter : forall x l l' r r' p, l <> r ->
+                                   Front u x l ->
                                    Front u x r ->
                                    l' --> p ->
                                    r' --> p ->
                                    set_In l (u l' x) ->
                                    set_In r (u r' x) ->
-                                   ~ set_In l (u p x) ->
-                                   ~ set_In r (u p x) ->
+                                   ~ set_In l (u r' x) ->
+                                   ~ set_In r (u l' x) ->
                                    Front u x p.
-                           
-  (*
-    Definition Disjoint {a b c d} (π : Path a b) (π' : Path c d) :=
-      forall p, (PathIn p π -> ~ PathIn p π') /\ (PathIn p π' -> ~ PathIn p π).
-*)
 
+  Definition DisjPaths {a b c d} (π : Path a b) (π' : Path c d) :=
+    forall p, (PathIn p π -> ~ PathIn p π') /\ (PathIn p π' -> ~ PathIn p π).
+
+  (*
+  Lemma unch_dom (u : Unch) x l l' :
+    set_In l (u l' x) -> forall q, is_def_lab x q -> forall (π : Path q l'), PathIn l π.
+  Proof.
+  Admitted.
+
+  Lemma unch_path_exist (u : Unch) x l l' r :
+    set_In l (u l' x) -> ~ set_In r (u l' x) -> exists (π : Path l l'), ~ PathIn r π.
+  Proof.
+  Admitted.
+    
+  Lemma front_and_join (u : Unch) x p :
+    Front u x p <-> is_def_lab x p \/ exists d d' q q' (π : Path d q) (π' : Path d' q'), is_def_lab x d /\
+                                                                                 is_def_lab x d' /\
+                                                                                 q --> p /\ q' --> p /\
+                                                                                 DisjPaths π π'.
+  Proof.
+    split; intros.
+    - induction H; [ left; assumption | right ].
+      eapply unch_path_exist in H6; try eassumption.
+      eapply unch_path_exist in H7; try eassumption.
+      destruct H6 as [π Hπ], H7 as [π' Hπ'].
+      specialize (unch_dom _ _ _ _ H4). intro Hdom.
+      specialize (unch_dom _ _ _ _ H5). intro Hdom'.
+      assert (Hdom := unch_dom _ _ _ _).
+      assert (DisjPaths π π'). {
+        unfold DisjPaths.
+        intros q.
+        split; intros.
+        - 
+
+      destruct IHFront1, IHFront2.
+      + exists l, r, l', r', π, π'.
+        do 4 (split; [ assumption |]).
+        unfold DisjPaths.
+        admit.
+      + 
+
+      assert  
       (*
-    Definition Join l ps :=
-      exists t t' f f' (π : Path t t') (π' : Path f f'), ps t t' π /\ ps f f' π' /\ Defs t /\ Defs f /\ 
-                                                    t <> f /\
-                                                    acyclic π /\
-                                                    acyclic π' /\
-                                                    Disjoint π π' /\
-                                                    t' --> l /\ f' --> l.
+      destruct IHFront1, IHFront2.
+      + exists l, r, l', r'.
+        eexists. eexists. 
+        do 4 (split; [ assumption |]).
+        admit.
+      +
+       *)
+      admit.
+    - admit.
+                           
   *)
 
   (* Paths *)
@@ -907,18 +961,20 @@ Module Uniana.
     intros.
     unfold unch_trans, unch_trans_ptw, unch_join_ptw, unch_trans_local in H.
     induction (preds to); simpl in *.
-    - inversion H.
-    - eapply set_inter_elim in H.
-      destruct H as [Hin Hother].
-      eapply IHl in Hother. clear IHl.
-      inject Hother; [ eauto |].
-      destruct (Lab_dec' u to); subst; eauto.
-      right.
-      intros.
-      inject H0; [ subst | eauto].
-      destruct (is_def x q to).
-      + simpl in Hin. firstorder.
-      + eauto using set_add_elim2.
+    - right. intros. inject H0.
+    - destruct (Lab_dec' to root).
+      + simpl in H. destruct H; [ subst | firstorder ]. eauto.
+      + eapply set_inter_elim in H.
+        destruct H as [Hin Hother].
+        eapply IHl in Hother. clear IHl.
+        inject Hother; [ eauto |].
+        destruct (Lab_dec' u to); subst; eauto.
+        right.
+        intros.
+        inject H0; [ subst | eauto].
+        destruct (is_def x q to).
+        * simpl in Hin. firstorder.
+        * eauto using set_add_elim2.
   Qed.
 
   Lemma unch_correct :
@@ -931,6 +987,27 @@ Module Uniana.
     unfold unch_concr in *.
     intros to i s u x.
     intros Hin Hunch.
+    destruct (Lab_dec' to root); subst.
+    - unfold unch_trans, unch_trans_ptw in Hunch. destruct (Lab_dec' root root); [ | firstorder ].
+      simpl in Hunch. destruct Hunch; [ | firstorder ]. subst.
+      exists i, s. eauto using precedes_self.
+    - assert (Hpred := Hin).
+      eapply in_exists_pred in Hpred; try eassumption.
+      destruct Hpred as [q [j [r [Hpredin Hpred]]]].
+      assert (Hedge: q --> to) by (eauto using step_conf_implies_edge).
+      eapply unch_trans_lem in Hunch; try eassumption.
+      destruct (Lab_dec' to u) as [ | Hneq ]; subst.
+      + exists i, s. split; [ | reflexivity ].  eauto using precedes_self.
+      + destruct Hunch; [ firstorder |].
+        eapply H in Hedge. destruct Hedge as [Hndef Huin].
+        edestruct Hconcr as [j' [r' [Hprec Heq]]]; eauto.
+        exists j', r'. rewrite Heq. eauto using precedes_succ, no_def_untouched.
+
+        split.
+        * eauto using precedes_succ. admit.
+        * rewrite Heq. eauto using no_def_untouched.
+
+    intros Hin Hunch Hdef.
     destruct (to == root).
     - rewrite e in *; clear e.
       exists i, s.
@@ -950,6 +1027,7 @@ Module Uniana.
         * eauto using precedes_succ.
         * rewrite Heq. eauto using no_def_untouched.
   Qed.
+  *)
 
   Definition lab_tag_matches (l : Lab) (j : Tag) (k : Conf) : bool :=
     match k with
@@ -1672,9 +1750,9 @@ Module Uniana.
         eapply join_orb_true_iff in H.
         destruct H as [u H].
         conv_bool.
-        destruct H as [Hneq [[Hneq' Huni] Hupi]].
-        specialize (HCunch p i s u x).
-        specialize (HCunch' p i s' u x).
+        destruct H as [Hunch [[Hneq' Huni] Hupi]].
+        specialize (HCunch p i s u x HIn Hunch).
+        specialize (HCunch' p i s' u x HIn' Hunch).
         destruct HCunch as [j [r [Hprec Heq]]]; try eassumption.
         destruct HCunch' as [j' [r' [Hprec' Heq']]]; try eassumption.
         rewrite <- Heq. rewrite <- Heq'.
