8b8073add4b2ca68a97723df6b7be8a3ef5a3e7e
Julian Rosemann <s9jnrose@stud.uni-saarland.de>
2019-10-25 23:26:04 +0200

    Progress on loop_splits step case

--- disj/LoopSplits.v
+++ disj/LoopSplits.v
@@ -66,26 +66,30 @@ Section disj.
     - admit.
   Admitted.
     
-    
-(*
-  Lemma s_deq_q : deq_loop s q1.
-  Proof.
-    rewrite last_common'_iff in Hlc. copy Hlc Hlc'. destructH' Hlc'.
-    do 2 rewrite nlcons_to_list in Hlc.
-    inversion Hpath1. inversion Hpath2.
-    path_simpl' H0.
-    path_simpl' H5.
-    do 2 rewrite nlcons_to_list in Hlc'.
-    eapply s_deq_q;eauto.
-    - admit.
-    - eapply eq_loop_exiting in Hexit1.
-      eapply eq_loop_exiting in Hexit2.
-      rewrite <-Hexit1, <-Hexit2. reflexivity.
-    - eapply tag_exit_iff in H3.
-      eapply tag_exit_iff in H5. *)
 End disj.
 
 Hint Resolve t1_tpath t2_tpath r_neq q1_eq_q2 tl_j_eq.
+
+Lemma impl_lift `(C : redCFG) (s q1' : Lab) (h q1 q2 e1 e2 : local_impl_CFG_type C q1')
+      (t1 t2 r1 r2 : list (Lab * Tag)) (k i j1 j2 : Tag)
+      (Heq : q1' = `q1)
+      (Hlc : last_common' ((`q1, j1) :< t1) ((`q2, j2) :< t2) r1 r2 (s, k))
+      (Hexit1 : exit_edge (`h) (`q1) (`e1))
+      (Hexit2 : exit_edge (`h) (`q2) (`e2))
+      (Hpath1 : TPath (root, start_tag) (`e1, i) ((`e1, i) :<: (`q1, j1) :< t1))
+      (Hpath2 : TPath (root, start_tag) (`e2, i) ((`e2, i) :<: (`q2, j2) :< t2))
+      (t1' := impl_tlist q1' t1)
+      (t2' := impl_tlist q1' t2)
+      (r1' := impl_tlist q1' r1)
+      (r2' := impl_tlist q1' r2)
+      (C' := local_impl_CFG C q1')
+  : exists s', last_common' ((q1,j1) :< t1') ((q2,j2) :< t2') r1' r2' (s',j1)
+          /\ exit_edge (redCFG:=C') h q1 e1
+          /\ exit_edge (redCFG:=C') h q2 e2
+          /\ TPath (C:=C') (↓ purify_implode q1', start_tag) (e1, i) ((e1, i) :<: (q1, j1) :< t1')
+          /\ TPath (C:=C') (↓ purify_implode q1', start_tag) (e2, i) ((e2, i) :<: (q2, j2) :< t2')
+          /\ depth s' = depth q1.
+Admitted.
   
 Theorem lc_disj_exits_lsplits' `{redCFG}
           (s e1 e2 q1 q2 h : Lab) (i j1 j2 k : Tag) (t1 t2 r1 r2 : list Coord)
@@ -105,7 +109,22 @@ Proof.
     { omega. }
     eapply deq_loop_depth.
     eapply s_deq_q;eauto.
-  - admit.
+  - assert (implode_nodes (head_exits_CFG H q1) q1 q1) as Hq1 by admit.
+    assert (implode_nodes (head_exits_CFG H q1) q1 q2) as Hq2 by admit.
+    assert (implode_nodes (head_exits_CFG H q1) q1 e1) as He1 by admit.
+    assert (implode_nodes (head_exits_CFG H q1) q1 e2) as He2 by admit.
+    assert (implode_nodes (head_exits_CFG H q1) q1 h) as Hh by admit.
+    cstr_subtype Hq1. cstr_subtype Hq2. cstr_subtype He1. cstr_subtype He2. cstr_subtype Hh.
+    eapply impl_lift in Hlc as Hlc';eauto.
+    destructH.
+    eapply lc_disj_exits_lsplits_base in Hlc'0. 2-7:eauto.
+    destruct Hlc'0 as [qq [qq' [Hlc'0|Hlc'0]]].
+    + setoid_rewrite splits'_spec. admit.
+      (* show that qq & qq' are exit_edges of s' on H, then construct the coresponding paths to them
+       * and show that last_common holds for these paths, bc they are subpaths. 
+       * then exploit IHn, use resulting qq_ih & qq'_ih as witnesses, choose the complicated case,
+       * there s',qq,qq' are the witnesses and you get one condition by IHn and the other by Hlc'0 *)
+    + (* analogous *) admit.
 Admitted.
   
 Theorem lc_disj_exits_lsplits `{redCFG}
