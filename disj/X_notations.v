55bac7b7ef3938f5ed3674c8b403636b3a196ff3
Julian Rosemann <s9jnrose@stud.uni-saarland.de>
2020-02-12 12:35:39 +0100

    Restructure build infrastructure

--- disj/X_notations.v
+++
@@ -1,9 +0,0 @@
-
-Context `{C : redCFG}.
-
-Infix "⊴" := Tagle (at level 70).
-
-Notation "p '-a>b' q" := (a_edge p q) (at level 55).
-Notation "p '-a>' q" := (p -a>b q = true) (at level 55).
-Notation "p '-->b' q" := (edge p q) (at level 55).
-Notation "p '-->' q" := (p -->b q = true) (at level 55, right associativity).
--- disj/X_vars.v
+++
@@ -1,9 +0,0 @@
-
-Variable (t1 t2 : ne_list (Lab * Tag)) (r1 r2 : list (Lab * Tag)) (q1 q2 s : Lab) (j1 j2 k : Tag).
-Hypotheses (Hlc : last_common' t1 t2 r1 r2 (s,k))
-           (Hpath1 : TPath (root,start_tag) (q1,j1) t1)
-           (Hpath2 : TPath (root,start_tag) (q2,j2) t2)
-(*           (Hneq : r1 <> r2) (* <-> r1 <> nil \/ r2 <> nil *)*)
-           (Hloop : eq_loop q1 q2)
-           (Htag : tl j1 = tl j2)
-           (Htagleq : hd 0 j1 <= hd 0 j2). 
