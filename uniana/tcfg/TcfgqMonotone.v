941378a30dda209249d4fc04eca80b32ee361499
Julian Rosemann <jtrosemann@cs.uni-saarland.de>
2020-09-14 18:09:54 +0200

    Sketch proof tcfg_fresh

--- uniana/tcfg/TcfgqMonotone.v
+++ uniana/tcfg/TcfgqMonotone.v
@@ -113,10 +113,63 @@ Section cfg.
           rewrite Q0 in Htagt0. lia.
   Qed.
 
+  Lemma non_entry_head_back_edge p h
+        (Hedge : edge__P p h)
+        (Hloop : loop_contains h p)
+    : p ↪ h.
+  Admitted.
+
   Lemma tcfg_fresh p i t
         (Hpath : TPath (p,i) (p,i) t)
-        (Hlen : 2 <= | t |)
+        (Hdep : | i | = depth p)
+        (Hlen : | t | >= 2)
     : False.
+  Proof.
+    eapply TPath_CPath in Hpath as Hpp.
+    eapply p_p_ex_head in Hpp.
+    2: rewrite map_length;eauto.
+    destructH.
+    destruct t. 1: inv Hpath.
+    eapply in_fst in Hpp0. destruct Hpp0 as [k Hpp0].
+    unfold TPath in *. path_simpl' Hpath.
+    eapply path_to_elem in Hpp0 as Hπ;eauto.
+    destruct Hπ as [π [Hπ Hπpre]].
+    inv_path Hπ.
+    1: {
+      inv_path Hpath.
+      - cbn in Hlen. lia.
+      - destruct x.
+        copy H0 H0'.
+        destruct H0' as [H0' _].
+        eapply non_entry_head_back_edge in H0'.
+        2: { eapply Hpp1. eapply path_contains_front in H. cbn. right.
+             eapply in_map with (f:=fst) in H. eapply H. }
+        eapply tag_back_edge_iff in H0';eauto.
+        admit. admit.
+    }
+    destruct x.
+    eapply path_from_elem in Hpp0 as Hϕ;eauto.
+    destruct Hϕ as [ϕ [Hϕ Hϕsuff]].
+    eapply weak_monotonicity in Hϕ;eauto.
+    3: {
+      intros. eapply loop_contains_deq_loop. eapply Hpp1. eapply postfix_incl;eauto.
+         Lemma prefix_map_fst (A B : Type) (l l' : list (A * B))
+               (Hpre: Prefix l l')
+           : Prefix (map fst l) (map fst l').
+         Admitted.
+         Lemma postfix_map_fst (A B : Type) (l l' : list (A * B))
+               (Hpre: Postfix l l')
+           : Postfix (map fst l) (map fst l').
+         Admitted.
+      eapply postfix_map_fst in Hϕsuff. assumption.
+    }
+    2: { eapply tag_depth_unroot in Hdep;eauto. }
+    eapply weak_monotonicity in H;eauto.
+    2: { intros. eapply loop_contains_deq_loop. eapply Hpp1. eapply prefix_incl;eauto.
+         eapply prefix_map_fst;eauto. eapply prefix_cons;eauto.
+    }
+    enough (take_r (depth h) t0 ◁ take_r (depth h) k) as Q.
+
   Admitted.
 
   Lemma tpath_tag_take_r_eq p i q j t h n
